beec8d48934caaf2a50582c18d2e0fc479959e2c
JACKRUNDLE <rundlej@oregonstate.edu>
2024-02-20 17:51:09 -0800

    main navigation template added

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "./routes/root";
+import Home from "./HomePage/homepage"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path="/" element={<Root />}>
+    <Route index element={<Home />} />
+  </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+
+
